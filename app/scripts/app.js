0c3fbd31d560505abbd3e1bc2b481a4f781a392c
Simon Egersand <s.egersand@gmail.com>
2015-01-02 00:24:35 +0100

    Indented code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,53 +10,53 @@
  */
 var app = angular
     .module('pickrandomApp', [
-      'ngResource',
-      'ngRoute',
-      'ngMessages',
-      'smoothScroll'
+        'ngResource',
+        'ngRoute',
+        'ngMessages',
+        'smoothScroll'
     ])
     .config(function ($routeProvider) {
-      $routeProvider
-          .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-          })
-          .when('/about', {
-            templateUrl: 'views/about.html',
-            controller: 'AboutCtrl'
-          })
-          .when('/pickmovie', {
-            templateUrl: 'views/pickmovie.html',
-            controller: 'PickmovieCtrl'
-          })
-          .when('/pickperson', {
-            templateUrl: 'views/pickperson.html',
-            controller: 'PickpersonCtrl'
-          })
-          .when('/loadNCalculateChoice', {
-            templateUrl: 'views/loadncalculatechoice.html',
-            controller: 'LoadncalculatechoiceCtrl'
-          })
-          .when('/specifyYourself', {
-            templateUrl: 'views/specifyyourself.html',
-            controller: 'SpecifyyourselfCtrl'
-          })
-          .otherwise({
-            redirectTo: '/'
-          });
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/pickmovie', {
+                templateUrl: 'views/pickmovie.html',
+                controller: 'PickmovieCtrl'
+            })
+            .when('/pickperson', {
+                templateUrl: 'views/pickperson.html',
+                controller: 'PickpersonCtrl'
+            })
+            .when('/loadNCalculateChoice', {
+                templateUrl: 'views/loadncalculatechoice.html',
+                controller: 'LoadncalculatechoiceCtrl'
+            })
+            .when('/specifyYourself', {
+                templateUrl: 'views/specifyyourself.html',
+                controller: 'SpecifyyourselfCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
     });
 
 app.run(function ($rootScope, $location) {
 
-  var history = [];
+    var history = [];
 
-  $rootScope.$on('$routeChangeSuccess', function() {
-    history.push($location.$$path);
-  });
+    $rootScope.$on('$routeChangeSuccess', function() {
+        history.push($location.$$path);
+    });
 
-  $rootScope.goBack = function () {
-    var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
-    $location.path(prevUrl);
-  };
+    $rootScope.goBack = function () {
+        var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+        $location.path(prevUrl);
+    };
 
 });
