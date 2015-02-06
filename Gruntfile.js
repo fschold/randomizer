685814a54e4ee40c16fab3af9961186c6719e91f
Simon Egersand <s.egersand@gmail.com>
2015-02-06 15:25:17 +0100

    Fix for images not showing

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,7 +174,8 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}', // Commented out because of Grunt not handling
+                                                                            // renaming of images correctly
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
